51f65cead6e538045c8f0121edfbf83aea061397
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2024-11-30 12:11:43 -0800

    Added: Microservice B and overhaul of CSS stylings

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function (next) {
   if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10); // Hash password before saving
+    this.password = await bcrypt.hash(this.password, 10);
   }
   next();
 });
