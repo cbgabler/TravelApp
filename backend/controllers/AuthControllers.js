fd3e33a9cecc588e84d3080c769ced4b668e5627
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-02-24 19:41:35 -0800

    Updated SignIN

--- backend/controllers/AuthControllers.js
+++ backend/controllers/AuthControllers.js
@@ -4,15 +4,28 @@ export const CreateUser = async (req, res) => {
     const { username, email, password } = req.body
 
     if ( !username || !email || !password ) {
-        return res.status(400).json({ error : "All fields are required" })
+        return res.status(400).json({ error : 'All fields are required' })
     }
 
     try {
         const newUser = new User({ username, email, password })
         await newUser.save()
         res.status(201).json(newUser)
-    } catch (error) {
-        res.status(500).json({ error : "Failed to create user" })
+    } catch ( error ) {
+        res.status(500).json({ error : 'Failed to create user' })
     }
 }
 
+export const GetUserByID = async (req, res) => {
+    const { id } = req.body
+
+    try {
+        const User = await User.findById(id)
+        if (!User) {
+            res.status(404).json({ error : 'User ID not found' })
+        }
+        res.status(200).json(User)
+    } catch (error) {
+        res.status(500).json({ error : 'Failed to find user' })
+    }
+}
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -6,55 +6,47 @@ import { RiLockPasswordLine } from "react-icons/ri";
 import '../styles/signin.css'
 
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [message, setMessage] = useState('');
+	const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/signin', {
-        email,
-        password,
-      });
-      setMessage('Sign in successful!');
-      localStorage.setItem('token', response.data.token);
-      navigate('/');
-    } catch (error) {
-      setMessage(error.response.data.message);
-    }
-  };
+		if( !email, !password ) {
+			
+		}
+	};
 
-  return (
-    <div className='form-container'>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className='form-control'>
-          <input
-            type="email"
-            placeholder="Enter your email or username"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <FaRegUser className='icon username'/>
-        </div>
-        <div className='form-control'>
-          <input
-            type="password"
-            placeholder="Enter your password"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <RiLockPasswordLine className='icon password'/>
-        </div>
-        <button type="submit">Login</button>
-        <p>Don't have an account? Please sign up</p>
-      </form>
-      {message && <p>{message}</p>}
-    </div>
-  );
+	return (
+		<div className='form-container'>
+		<h2>Login</h2>
+		<form onSubmit={handleSubmit}>
+			<div className='form-control'>
+			<input
+				type="email"
+				placeholder="Enter your email or username"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<FaRegUser className='icon username'/>
+			</div>
+			<div className='form-control'>
+			<input
+				type="password"
+				placeholder="Enter your password"
+				value={password}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<RiLockPasswordLine className='icon password'/>
+			</div>
+			<button type="submit">Login</button>
+			<p>Don't have an account? Please sign up</p>
+		</form>
+		{message && <p>{message}</p>}
+		</div>
+	);
 }
 
 export default SignIn;
