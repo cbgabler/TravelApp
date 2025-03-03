c7d33af89c2a4b0172abe8fd838634fa4cea1d04
Carson Gabler <102882837+cbgabler@users.noreply.github.com>
2025-03-02 20:52:58 -0800

    Wip

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+// Needs Work
 function SignUp() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
